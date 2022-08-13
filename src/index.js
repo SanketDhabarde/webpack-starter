73083d260c18ff1fe8252d2957a5acc1c9138555
SanketDhabarde <sanketdhabarde13@gmail.com>
2022-08-13 17:04:14 +0530

    [Feat]: Add img asset

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import App from "./App";
+import Logo from "./logo.svg";
 
 const root = document.getElementById("root");
 
 root.innerHTML = `
+<img src="${Logo}" alt="build icon" />
 <h1>Learning web tooling</h1>
 ${App({ name: "Sanket" })}
 `;
